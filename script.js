5d9f793d269e669946a91302c7045672dc301126
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-06-01 00:36:50 +0530

    About Section Style And Configurations

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+// Active Menu Button On Navigation Bar (Header Section)
+
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 }
+//  Navigation In Other Videos On About Section
+
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn => {
+    btn.onclick = () => {
+        let src = btn.getAttribute('data-src');
+        document.querySelector('.about .video-container .video').src = src;
+    }
+})
